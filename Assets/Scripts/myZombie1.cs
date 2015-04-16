2470cdb87d02e0be3c772a96827944fe14d6b50d
unknown <rlam16@G013210-BDP09D.coast.cccd.adx>
2015-04-15 21:36:05 -0700

    renamed script

--- Assets/Scripts/myZombie1.cs
+++ Assets/Scripts/myZombie1.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Default : MonoBehaviour {
+public class myZombie1 : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
